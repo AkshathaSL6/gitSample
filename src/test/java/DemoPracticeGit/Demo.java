d204c05089d208022d53cc4216492c1d43419296
rajath <rajath@rajath>
2023-08-04 14:59:00 +0530

    Demo is committed

---
+++ src/test/java/DemoPracticeGit/Demo.java
@@ -0,0 +1,12 @@
+package DemoPracticeGit;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void gitpractice() {
+		System.out.println("in demo practice github");
+	}
+
+}
